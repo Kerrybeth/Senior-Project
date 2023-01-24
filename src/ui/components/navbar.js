731a7910b4fecb3345dd5487b6135d0eb2f8e883
Risemon2 <82181200+Risemon2@users.noreply.github.com>
2023-01-24 11:36:24 -0500

    Made Skeleton Navbar
    Made Navbar component, doesn't have links rn.

---
+++ src/ui/components/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class NavbarComp extends React.Component{
+	
+render(){
+  return (
+	<ul>
+		<li>
+			<button class="navbutton">User</button>
+			<button class="navbutton">Groups</button>
+			<button class="navbutton">Events</button>
+			<button class="navbutton">Contacts</button>
+			<button class="navbutton">Notifications</button>
+		</li>
+	</ul>
+	);
+}
+}
+
+export default NavbarComp;
