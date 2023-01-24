472269cb812f25ee85433f2789da4b33c2425049
Logan T <75921276+alumnu@users.noreply.github.com>
2023-01-24 11:51:46 -0500

    Fixed format of App.js
    Moved home components to Home.js and removed extra App.js stuff to accommodate it

---
+++ src/ui/home/Home.js
@@ -0,0 +1,16 @@
+import EventCarousel from './EventCarousel';
+
+import '../../App.css';
+
+function Home() {
+    return (
+    <div className="App">
+        <div id="Home">
+            <EventCarousel></EventCarousel>
+        </div>
+    </div>
+    );
+  }
+  
+  export default Home;
+  
