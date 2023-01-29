3d7143f26103dc568813ee4895c2ccf2b85bebe1
Stephane Katende <swar.kung@gmail.com>
2023-01-28 19:46:34 -0500

    side bar added, home route working

---
+++ src/ui/layout+routes/appRoutes.tsx
@@ -0,0 +1,120 @@
+import Home from "../home/Home";
+import { RouteType } from "../components/configs";
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+
+
+const appRoutes: RouteType[] = [
+    {
+        index: true,
+        element: <Home />,
+        state: "home"
+    },
+];
+
+// const appRoutes1: RouteType[] = [
+//     {
+//         index: true,
+//         element: <HomePage />,
+//         state: "home"
+//     },
+//     {
+//         path: "/installation",
+//         element: <InstallationPage />,
+//         state: "installation",
+//         sidebarProps: {
+//             displayText: "Installation",
+//             icon: <FileDownloadOutlinedIcon />
+//         }
+//     },
+//     {
+//         path: "/dashboard",
+//         element: <DashboardPageLayout />,
+//         state: "dashboard",
+//         sidebarProps: {
+//             displayText: "Dashboard",
+//             icon: <DashboardOutlinedIcon />
+//         },
+//         child: [
+//             {
+//                 index: true,
+//                 element: <DashboardIndex />,
+//                 state: "dashboard.index"
+//             },
+//             {
+//                 path: "/dashboard/default",
+//                 element: <DefaultPage />,
+//                 state: "dashboard.default",
+//                 sidebarProps: {
+//                     displayText: "Default"
+//                 },
+//             },
+//             {
+//                 path: "/dashboard/analytics",
+//                 element: <AnalyticsPage />,
+//                 state: "dashboard.analytics",
+//                 sidebarProps: {
+//                     displayText: "Analytic"
+//                 }
+//             },
+//             {
+//                 path: "/dashboard/saas",
+//                 element: <SaasPage />,
+//                 state: "dashboard.saas",
+//                 sidebarProps: {
+//                     displayText: "Saas"
+//                 }
+//             }
+//         ]
+//     },
+//     {
+//         path: "/component",
+//         element: <ComponentPageLayout />,
+//         state: "component",
+//         sidebarProps: {
+//             displayText: "Components",
+//             icon: <AppsOutlinedIcon />
+//         },
+//         child: [
+//             {
+//                 path: "/component/alert",
+//                 element: <AlertPage />,
+//                 state: "component.alert",
+//                 sidebarProps: {
+//                     displayText: "Alert"
+//                 },
+//             },
+//             {
+//                 path: "/component/button",
+//                 element: <ButtonPage />,
+//                 state: "component.button",
+//                 sidebarProps: {
+//                     displayText: "Button"
+//                 }
+//             }
+//         ]
+//     },
+//     {
+//         path: "/documentation",
+//         element: <DocumentationPage />,
+//         state: "documentation",
+//         sidebarProps: {
+//             displayText: "Documentation",
+//             icon: <ArticleOutlinedIcon />
+//         }
+//     },
+//     {
+//         path: "/changelog",
+//         element: <ChangelogPage />,
+//         state: "changelog",
+//         sidebarProps: {
+//             displayText: "Changelog",
+//             icon: <FormatListBulletedOutlinedIcon />
+//         }
+//     }
+// ];
+
+export default appRoutes;
