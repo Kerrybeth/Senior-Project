5dfa966738efa3d2abac715c3368e75a66583d99
Kerrybeth Gorman <kerrybethg@gmail.com>
2023-01-31 11:13:37 -0500

    adding buttons for options

--- src/ui/settings/Settings.js
+++ src/ui/settings/Settings.js
@@ -6,14 +6,25 @@ const Settings = () => {
         <Box
             sx={{
                 display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
+                justifyContent: 'left',
+                alignItems: 'left',
                 minHeight: '30vh',
             }}
         >
-            <Typography variant="h1" style={{ color: 'black' }}>
-             Settings!
-            </Typography>
+            <Typography variant="h1" style={{ color: 'black' }}> Settings </Typography>
+            <Typography variant="h2" style={{ color: 'black' }}> Schedule Privacy </Typography>
+                <button> Friends </button> 
+                <button> Group Members</button>
+                <button> All Users </button>
+                <button> Nobody </button>
+            <Typography variant="h2" style={{ color: 'black' }}> Theme </Typography>
+                <button> Light </button>
+                <button> Dark </button>
+            <Typography variant="h2" style={{ color: 'black' }}> Account Settings </Typography>
+                <button> Change Username </button>
+                <button> Change Password </button>
+                <button> Delete Account </button>
+
         </Box>
     );
 }
