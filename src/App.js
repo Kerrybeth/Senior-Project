d238dc4d9d4e57ee8d8c94c2f0068a5cf3c9aa48
anxelic <chrisinfinityy@gmail.com>
2023-02-02 10:56:45 -0500

    log in fully set up and working with firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import Home from './ui/home/Home';
-import Navbar from './ui/components/Navbar.js';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './ui/login/login';
+import Signup from './ui/login/signup';
+import { Routes, Route} from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import { UserAuthContextProvider } from './ui/auth/UserAuthContext';
 
 function App() {
   return (
     <div>
-      <Home />
-      <Navbar />
+      <Container>
+        <Row>
+          <Col>
+          <UserAuthContextProvider>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </UserAuthContextProvider>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/ui/home/Home.js
+++ src/ui/home/Home.js
@@ -1,16 +1,33 @@
-import EventCarousel from './EventCarousel';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router";
+import { useUserAuth } from "../auth/UserAuthContext";
 
-import '../../App.css';
+const Home = () => {
+  const { logOut, user } = useUserAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <>
+      <div className="p-4 box mt-3 text-center">
+        Hello Welcome <br />
+        {user && user.email}
+      </div>
+      <div className="d-grid gap-2">
+        <Button variant="primary" onClick={handleLogout}>
+          Log out
+        </Button>
+      </div>
+    </>
+  );
+};
 
-function Home() {
-    return (
-    <div className="App">
-        <div id="Home">
-            <EventCarousel></EventCarousel>
-        </div>
-    </div>
-    );
-  }
-  
-  export default Home;
+export default Home;
   
