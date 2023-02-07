c772873b46e8130d37f334a4dad4c9c53bde0627
anxelic <chrisinfinityy@gmail.com>
2023-02-07 11:42:09 -0500

    got calendar working on home page

---
+++ src/ui/components/calendar.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import '../../App.css';
+
+
+export default class Calendar extends React.Component {
+    state = {
+        currentEvents: []
+    }
+
+    render() {
+        return (
+        <FullCalendar
+            plugins={[ dayGridPlugin, interactionPlugin, timeGridPlugin ]}
+            initialView="dayGridMonth"
+            dateClick={this.handleDateClick}
+            editable={true}
+            eventContent={renderEventContent}
+            eventsSet={this.handleEvents}
+        />
+    )
+    }
+
+    handleDateClick = (arg) => {
+        //eventadd(arg);
+        //this.handleRefresh();
+        // let calendarApi = arg.view.calendar;
+        // calendarApi.addEvent({
+        //     title: 'test',
+        //     start: arg.dateStr,
+        //     end: arg.dateStr,
+        //     allday: arg.allday
+        // });
+        //alert('clicked');
+        
+    }
+
+    handleRefresh = () => {
+        this.setState({});
+    }
+
+    handleEvents = (events) => {
+        this.setState({
+            currentEvents: events
+        })
+    }
+
+}
+
+function renderEventContent (eventInfo) {
+    return (
+        <>
+            <b>{eventInfo.timeText}</b>
+            <i>{eventInfo.title}</i>
+        </>
+    )
+}
+
+function eventadd (arg) {
+    return (
+        <>
+            <p>
+                testing
+            </p>
+        </>
+    )
+}
--- src/ui/home/Home.js
+++ src/ui/home/Home.js
@@ -1,13 +1,15 @@
 import EventCarousel from './EventCarousel';
 
 import '../../App.css';
+import FullCalendar from '@fullcalendar/react';
+import Calendar from '../components/calendar';
 
 function Home() {
     return (
     <div className="App">
         <div className="pageLight">
             <EventCarousel></EventCarousel>
-            <div id="calendar">CALENDAR HERE</div>
+            <div id="calendar"><Calendar /></div>
         </div>
     </div>
     );
