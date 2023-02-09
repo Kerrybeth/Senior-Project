dc83aca505d4d554cd687d201be38313253df699
Logan T <75921276+alumnu@users.noreply.github.com>
2023-02-09 11:05:30 -0500

    Fixed settings, added calendar to home

--- src/ui/settings/Settings.js
+++ src/ui/settings/Settings.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-        function AutoLayoutExample() {
+function Settings() {
             return (
               <Container>
                 <Row>
@@ -29,6 +29,6 @@ import Col from 'react-bootstrap/Col';
                 </Row>
               </Container>
             );
-          }
+}
 
 export default Settings; 
