0fbb85d80b5fdd6ca2d35ccc754580340a06efa3
Kerrybeth Gorman <kerrybethg@gmail.com>
2023-02-14 10:53:51 -0500

    this spacing is better

--- src/ui/settings/Settings.js
+++ src/ui/settings/Settings.js
@@ -4,8 +4,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-//import ButtonGroup from 'react-bootstrap/ButtonGroup';
-//import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 
 function Settings() {
   return (
@@ -76,9 +74,9 @@ function Settings() {
       <Row>
         <Col><Typography variant="h5" style={{ color: 'black' }}> Account Settings </Typography></Col>
         <div class="d-grid gap-3">
-            <Button type="button" class="btn btn-outline-primary">Change Username</Button>
-            <Button type="button" class="btn btn-outline-primary">Change Password</Button>
-            <Button type="button" class="btn btn-outline-primary">Delete Account</Button>
+          <Button type="button" class="btn btn-outline-primary">Change Username</Button>
+          <Button type="button" class="btn btn-outline-primary">Change Password</Button>
+          <Button type="button" class="btn btn-outline-primary">Delete Account</Button>
         </div>
       </Row>
     </Container>
