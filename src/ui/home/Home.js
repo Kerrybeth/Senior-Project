d523084a2f9cf332a3fb5c961a00ad6b126768d2
anxelic <chrisinfinityy@gmail.com>
2023-03-21 11:30:11 -0400

    messing with not allowing unlogged users in

--- src/ui/home/Home.js
+++ src/ui/home/Home.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { useUserAuth } from "../auth/UserAuthContext";
 import EventCarousel from "./EventCarousel";
 import { Calendar } from "../components/calendar";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Home = () => {
 
   const { user } = useUserAuth();
+  const navigate = useNavigate();
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+    } else {
+      navigate("/login")
+    }
+  });
+
 
   return (
     <>
