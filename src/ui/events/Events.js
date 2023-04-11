e044ff65faca3f2d930355283019a019cf9ea1d0
Risemon2 <82181200+Risemon2@users.noreply.github.com>
2023-04-11 11:35:42 -0400

    Update Events.js
    Implemented firebase functionality so it can read from the events db and display.
    
    Need to add a limiter so the tabs can show different events based on the current day, week, and month.

--- src/ui/events/Events.js
+++ src/ui/events/Events.js
@@ -3,8 +3,39 @@ import Tabs from 'react-bootstrap/Tabs';
 import { Link } from "react-router-dom";
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import { lightTheme } from '../../theme';
+import { useTheme } from '@mui/material/styles';
+import { useContext, useState, useEffect } from 'react';
+import { userAuthContext } from '../auth/UserAuthContext';
+import { getDatabase, ref, query, set, push, onValue} from "firebase/database";
+import { getAuth, currentUser } from 'firebase/auth';
+
 
 const Events = () => {
+	const theme = useTheme(); 
+	const user = useContext(userAuthContext) || '';
+	let eventsTemp = [];
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        const db = getDatabase();  
+        const dataRef = ref(db, 'users/' + user.user.uid + '/events');
+		
+        onValue(dataRef, (snapshot) => {
+            snapshot.forEach(childSnapshot => {
+                let title = childSnapshot.val().title;
+                let start = childSnapshot.val().start;
+                let end = childSnapshot.val().end;
+				let location = childSnapshot.val().location;
+
+                eventsTemp.push({"title": title, "start": start, "end": end, "location": location});
+            });
+
+            setEvents(eventsTemp);
+            eventsTemp = [];
+        });
+    }, [user]);
+	
     return (
         <div className="pageLight">
 			<div className="tabList">
@@ -13,45 +44,63 @@ const Events = () => {
 				id="events-tabs"
 				className="mb-3"
 				>
-					<Tab eventKey="first" title="Daily">
+					<Tab eventKey="first" title="Recent"> {/* Got it to display the recieved data, need to implement recent, weekly, and monthly settings. */}
 						<ListGroup>
-							<ListGroup.Item>
-								<div>
-									<h2>Events Name 1</h2>
-									Start and End Time:
-									<br />
-									Location: 
-								</div>
-							</ListGroup.Item>
+							{events.map((events) => {
+								return(
+								<ListGroup.Item>
+									<div>
+										<h2>{events.title}</h2>
+										Start Date and Time: {events.start}
+										<br />
+										End Date and Time: {events.end}
+										<br />
+										Location: {events.location}
+									</div>
+								</ListGroup.Item>
+								)
+							})}
 						</ListGroup>
 					</Tab>
 					<Tab eventKey="second" title="Weekly">
 						<ListGroup>
-							<ListGroup.Item>
-								<div>
-									<h2>Events Name 1</h2>
-									Date and Time:
-									<br />
-									Location: 
-								</div>
-							</ListGroup.Item>
+							{events.map((events) => {
+								return(
+								<ListGroup.Item>
+									<div>
+										<h2>{events.title}</h2>
+										Start Date and Time: {events.start}
+										<br />
+										End Date and Time: {events.end}
+										<br />
+										Location: {events.location}
+									</div>
+								</ListGroup.Item>
+								)
+							})}
 						</ListGroup>
 					</Tab>
 					<Tab eventKey="third" title="Monthly">
 						<ListGroup>
-							<ListGroup.Item>
-								<div>
-									<h2>Events Name 1</h2>
-									Date and Time:
-									<br />
-									Location: 
-								</div>
-							</ListGroup.Item>
+							{events.map((events) => {
+								return(
+								<ListGroup.Item>
+									<div>
+										<h2>{events.title}</h2>
+										Start Date and Time: {events.start}
+										<br />
+										End Date and Time: {events.end}
+										<br />
+										Location: {events.location}
+									</div>
+								</ListGroup.Item>
+								)
+							})}
 						</ListGroup>
 					</Tab>
 				</Tabs>
 			</div>
-			<Link to="/CreateEvents">
+			<Link to="/CreateEvents"> {/* Tried and failed to implement the theme on the buttons in this page. Did not like Mui buttons being used. */}
 				<Button variant="secondary">
 					Create Event
 				</Button>
