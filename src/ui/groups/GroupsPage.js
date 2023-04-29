33d0bd07112c890365df4331d9c6b5bb0dec65a9
alumnu <peppydizzy@gmail.com>
2023-04-28 22:34:13 -0400

    just messing with stuff, commented out

--- src/ui/groups/GroupsPage.js
+++ src/ui/groups/GroupsPage.js
@@ -2,8 +2,14 @@ import { Link, useLocation } from "react-router-dom";
 import { getDatabase, ref, onValue } from "firebase/database";
 import { useEffect, useState } from "react";
 import '../../App.css';
+import Form from 'react-bootstrap/Form';
+import { useSelector } from 'react-redux';
 
 const GroupsPage = () => {
+
+    const { user, error, sucess } = useSelector(
+		(state) => state.user
+	)
 
     const location = useLocation(); // React Hook
     const groupId = (location.pathname).slice(8);
@@ -13,6 +19,7 @@ const GroupsPage = () => {
     const dataRef = ref(db, `groups/${groupId}`);
     const [name, setName] = useState("");
     const [desc, setDesc] = useState("");
+    //const [contacts, setContacts] = useState([]);
   
     useEffect(() => {
         onValue(dataRef, (snapshot) => {
@@ -21,12 +28,33 @@ const GroupsPage = () => {
             setDesc(group.desc);
         });
     }, [dataRef]);
+
+    // function getContacts() {
+    //     onValue(ref(db, 'users/' + user.uid + '/contacts'), (snapshot) => {
+    //         snapshot.forEach(childSnapshot => {
+    //             setContacts(childSnapshot.val().name);
+    //         });
+    //     });
+    // }
+
+    // function Option() {
+    //     return (
+    //         <div>
+    //         {contacts.map((name) => (
+    //             <option value={name}>{name}</option>
+    //         ))}
+    //         </div>
+    //     );
+    // }
   
     return (
         <div className="group-page">
             <h1>{name}</h1>
             <p>{desc}</p>
             <Link to="/groups">Back to Groups</Link>
+            {/* <Form.Select aria-label="Default select example" onClick={getContacts}>
+                <Option />
+            </Form.Select> */}
         </div>
     );
 }
