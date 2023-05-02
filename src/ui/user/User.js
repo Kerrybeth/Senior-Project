a72ec19862068b69783bd946426a8f8fe6680778
Logan T <75921276+alumnu@users.noreply.github.com>
2023-05-02 11:29:16 -0400

    added profile picture

--- src/ui/user/User.js
+++ src/ui/user/User.js
@@ -1,23 +1,34 @@
 import { Box } from "@mui/system";
 import Typography from '@mui/material/Typography';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Image from 'react-bootstrap/Image';
 import ListGroup from 'react-bootstrap/Listgroup';
 import Button from '@mui/material/Button';
 import { getDatabase, ref, onValue, set, update } from "firebase/database";
 import { useEffect, useState } from 'react';
 import { useSelector } from "react-redux";
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 const User = () => {
 	const { user, error, sucess } = useSelector(
 		(state) => state.user
 	)
+
 	const [data, setData] = useState("");
 	const [timeRanges, setTimeRanges] = useState([]);
 
+	const [img, setImg] = useState('');
+	
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const db = getDatabase(); 
+
 	useEffect(() => {
 
-        const db = getDatabase();  
+        //const db = getDatabase();  
         const dataRef = ref(db, 'users/' + user.uid + '/profile');
 		const dataRefAv = ref(db, 'users/' + user.uid + '/profile' + '/availability' + '/availability');
 
@@ -32,6 +43,11 @@ const User = () => {
 				let bio = "";
 				update(ref(db, 'users/' + user.uid + '/profile'), {
 					bio: bio
+				});
+			} else if (data.image == null) {
+				let image = "";
+				update(ref(db, 'users/' + user.uid + '/profile'), {
+					image: image
 				});
 			} else {
 				setData(data);
@@ -58,11 +74,36 @@ const User = () => {
 
     }, []);
 
+	function handleImage(event) {
+		event.preventDefault();
+		alert("hi");
+
+		//const db = getDatabase(); 
+		update(ref(db, 'users/' + user.uid + '/profile'), {
+			image: img,
+        });
+
+		setImg('');
+	}
+
 	return (
 		<div>
-			<Box component='button' style={{ minHeight: '150px', minWidth: '150px', position: 'relative', top: '100px' }}>
-				<Image src="logo.svg" roundedCircle />
+			<Box component='button' sx={{border: '0', backgroundColor: 'transparent'}} onClick={handleShow} style={{ minHeight: '150px', minWidth: '150px', position: 'relative', top: '100px', maxHeight:'150px', maxWidth: '150px' }}>
+				<Image id='profilepic' src={data.image} roundedCircle />
 			</Box>
+			<Modal show={show} onHide={handleClose} centered>
+				<Modal.Header closeButton>
+          			<Modal.Title>Paste a link to an image</Modal.Title>
+        		</Modal.Header>
+				<Modal.Body>
+					<Form onSubmit={handleImage}>
+					<Form.Control type="text" value={img} onChange={(event) => setImg(event.target.value)}/>
+					<Button variant="primary" type='submit' style={{padding: '10px'}} onClick={handleClose}>
+            			Save Changes
+          			</Button>
+					</Form>
+				</Modal.Body>
+			</Modal>
 			<ListGroup style={{
 				display: 'flex',
 				minHeight: '100px',
@@ -75,7 +116,8 @@ const User = () => {
 					<Typography variant="h3" style={{
 						color: 'black',
 						justifyContent: 'left',
-						alignItems: 'left'
+						alignItems: 'left',
+						overflowWrap: 'break-word'
 					}}>
 						Name: {data.name}
 						<br />
